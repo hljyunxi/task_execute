abefebbc2a8103faba688a5360119d39d87f4cd4
hljyunxi <hljyunxi@126.com>
2014-05-30 19:40:26 +0800

    parse job from yaml file

--- src/job/__init__.py
+++ src/job/__init__.py
@@ -8,12 +8,40 @@ class JobRunner(object):
     def __init__(self,
         host_list = const.DEFAULT_HOST_LIST,
         setup_cache = const.DEFAULT_SETUP_CACHE,
+        sudo = False,
+        sudo_user = const.DEFAULT_SUDO_USER,
+        extra_vars = None,
         trigger = None):
+
         this.setup_cache = setup_cache
         self.trigger = trigger
 
     def _do_setup_step(self, job):
         pass
 
-    def _load_job_from_file(self, job_file):
+    def load_job_from_job_conf(self, job_conf):
+        if type(job_conf) != dict:
+            raise errors.JobError('job conf data must be dict')
+
+        job = None
+        if 'include' in job_conf:
+            if len(job_conf) != 1:
+                raise errors.JobError('job conf error: include should only have\
+                        only one item')
+            else:
+                job = self.load_job_from_file(job_conf['include'])
+        else:
+            job = Job(self, job_conf)
+
+        return job
+
+    def load_job_from_file(self, job_file):
+        yaml_data = utils.parse_yaml_from_file(job_file)
+        assert type(yaml_data) is dict, "job yaml file should be dict"
+        return Job(self, yaml_data)
+
+    def run(self):
+        pass
+
+    def _run_task(self):
         pass
