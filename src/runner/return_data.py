c0acbdd0e4013a01d9e16d1714f21240cd39201b
hljyunxi <hljyunxi@126.com>
2014-06-01 15:48:03 +0800

    add redis && ssdb store

--- src/runner/return_data.py
+++ src/runner/return_data.py
@@ -3,4 +3,10 @@
 #Author: chenyunyun<hljyunxi@gmail.com>
 
 class ReturnData(object):
-    pass
+    def __init__(self, conn=None, host=None, result=None, comm_ok=True):
+        self.conn = conn
+        self.host = host
+        self.result = result
+        self.comm_ok = comm_ok
+
+
