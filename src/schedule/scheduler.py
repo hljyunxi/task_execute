5499f2e335502d278c32b6220721bf643da81d71
hljyunxi <hljyunxi@126.com>
2014-06-05 22:28:39 +0800

    add scheduler framework

--- src/schedule/scheduler.py
+++ src/schedule/scheduler.py
@@ -2,3 +2,29 @@
 #coding: utf8
 #Author: chenyunyun<hljyunxi@gmail.com>
 
+from threading import Thread, Event, Lock
+
+class Scheduler(object):
+    def __init__(self, ):
+        pass
+
+    def initialize(self):
+        pass
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def _add_job(self):
+        pass
+
+    def add_job_store(self):
+        pass
+
+    def _main_loop(self):
+        pass
+
+    def _process_job(self):
+        pass
